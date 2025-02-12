dd5ea6971423e0a0256eb5edd54ad54f7a4c67d8
ztwww2222 <130041523+ztwww2222@users.noreply.github.com>
2025-02-13 00:10:25 +0800

    Update dial.go

--- edgediscovery/dial.go
+++ edgediscovery/dial.go
@@ -1,5 +1,4 @@
 package edgediscovery
-
 import (
 	"context"
 	"crypto/tls"
@@ -7,9 +6,10 @@ import (
 	"time"
 
 	"github.com/pkg/errors"
+	"golang.org/x/net/proxy"
 )
 
-// DialEdge makes a TLS connection to a Cloudflare edge node
+// DialEdgeWithH2Mux makes a TLS connection to a Cloudflare edge node
 func DialEdge(
 	ctx context.Context,
 	timeout time.Duration,
@@ -26,34 +26,44 @@ func DialEdge(
 		dialer.LocalAddr = &net.TCPAddr{IP: localIP, Port: 0}
 	}
 	edgeConn, err := dialer.DialContext(dialCtx, "tcp", edgeTCPAddr.String())
+	proxyDialer := proxy.FromEnvironmentUsing(&dialer)
+
+	var edgeConn net.Conn
+	var err error
+
+	ctxDialer, ok := proxyDialer.(interface {
+		DialContext(context.Context, string, string) (net.Conn, error)
+	})
+	if ok {
+		// Inherit from parent context so we can cancel (Ctrl-C) while dialing
+		dialCtx, dialCancel := context.WithTimeout(ctx, timeout)
+		defer dialCancel()
+		edgeConn, err = ctxDialer.DialContext(dialCtx, "tcp", edgeTCPAddr.String())
+	} else {
+		edgeConn, err = proxyDialer.Dial("tcp", edgeTCPAddr.String())
+	}
 	if err != nil {
 		return nil, newDialError(err, "DialContext error")
 	}
-
 	tlsEdgeConn := tls.Client(edgeConn, tlsConfig)
 	tlsEdgeConn.SetDeadline(time.Now().Add(timeout))
-
 	if err = tlsEdgeConn.Handshake(); err != nil {
 		return nil, newDialError(err, "TLS handshake with edge error")
 	}
-	// clear the deadline on the conn; http2 has its own timeouts
+	// clear the deadline on the conn; h2mux has its own timeouts
 	tlsEdgeConn.SetDeadline(time.Time{})
 	return tlsEdgeConn, nil
 }
-
 // DialError is an error returned from DialEdge
 type DialError struct {
 	cause error
 }
-
 func newDialError(err error, message string) error {
 	return DialError{cause: errors.Wrap(err, message)}
 }
-
 func (e DialError) Error() string {
 	return e.cause.Error()
 }
-
 func (e DialError) Cause() error {
 	return e.cause
 }
